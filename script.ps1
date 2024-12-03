499300eb94f58fa97d46995db2f050999afb74c7
Pedro Prieto <pedpral@gmail.com>
2024-12-03 23:01:36 +0100

    Borrar usuarios antiguos

--- script.ps1
+++ script.ps1
@@ -1,3 +1,18 @@
-"hola mundo"
-New-Item c:\testtesttest2 -type directory
-"Nuevo test"
+# "hola mundo"
+New-Item c:\test3 -type directory
+# "Nuevo test"
+
+# Borrar usuarios que no se conectan hace más de 100 días
+$users = Get-LocalUser | Where-Object {$_.description -eq "pGina created" -and $_.Lastlogon -le (Get-Date).AddDays(-100)} | Select-Object Name,SID,Lastlogon 
+
+foreach ($user in $users) {
+  #Eliminar cuenta
+  Remove-LocalUser -Name $user.name
+  
+  # Eliminar carpeta personal
+  $sid = $user.sid
+  Get-WmiObject Win32_UserProfile -Filter "sid='${sid}'" | ForEach-Object {
+    $_.Delete()
+  }
+
+}
