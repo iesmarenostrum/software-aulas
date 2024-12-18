5ab229b6b690d1cc0b5215711cb6641859106e8e
Pedro Prieto <pedpral@gmail.com>
2024-12-18 19:06:58 +0100

    Quito dir

--- script.ps1
+++ script.ps1
@@ -1,7 +1,3 @@
-# "hola mundo"
-New-Item c:\test3 -type directory
-# "Nuevo test"
-
 # Borrar usuarios que no se conectan hace más de 100 días
 $users = Get-LocalUser | Where-Object {$_.description -eq "pGina created" -and $_.Lastlogon -le (Get-Date).AddDays(-100)} | Select-Object Name,SID,Lastlogon 
 
