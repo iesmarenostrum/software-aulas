386ef2a1078948ef94725aa7e2f5ea4e0af68e34
Pedro Prieto <pedpral@gmail.com>
2025-01-20 13:11:56 +0100

    Typo

--- aula15.ps1
+++ aula15.ps1
@@ -5,6 +5,3 @@ If(-Not (Test-Path -Path "c:\Program Files\Cisco Packet Tracer 8.2.2\bin\PacketT
   "Packet Tracer"
   invoke-expression -Command "cmd /c \\aula15profesor\compartida\software\Packet_Tracer822_64bit_setup_signed.exe /verysilent"
 }
-
-
-}
