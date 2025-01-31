83553e4696bdaae0e134c3e105dbfebfbd515256
Pedro Prieto <pedpral@gmail.com>
2025-01-31 13:58:06 +0100

    Veyon aula23

--- aula23.ps1
+++ aula23.ps1
@@ -4,3 +4,24 @@ $urlSand = "https://raw.githubusercontent.com/iesmarenostrum/software-aulas/refs
 curl -o "$fileSand" "$urlSand"
 
 invoke-expression -Command "$fileSand"
+
+
+
+If(-Not (Test-Path -Path "c:\veyoninstalado.txt")) {
+  $fileVeyon = "c:\windows\temp\veyonsetup.exe"
+  $urlVeyon = "https://github.com/veyon/veyon/releases/download/v4.9.2/veyon-4.9.2.0-win64-setup.exe"
+  curl -o "$fileVeyon" "$urlVeyon"
+  invoke-expression -Command "${fileVeyon} /S /NoMaster /NoStartMenuFolder /Service /InterceptionDriver"
+
+
+
+  $fileCert= "c:\windows\temp\profesor_public_key.pem"
+  $urlCert= "https://raw.githubusercontent.com/iesmarenostrum/software-aulas/refs/heads/main/profesor_public_key.pem"
+
+  curl -o "$fileCert" "$urlCert"
+
+
+  invoke-expression -Command "cmd /c 'C:\Program Files\veyon\veyon-cli.exe' authkeys import Profesor ${fileCert}"
+
+  echo veyoninstalado> c:/veyoninstalado.txt
+}
