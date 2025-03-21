e107b1ae47dcae2ee27626f7f8733af773ecb133
Pedro Prieto <pedpral@gmail.com>
2025-03-21 13:02:47 +0100

    Veyon

--- aula23.ps1
+++ aula23.ps1
@@ -23,5 +23,25 @@ If(-Not (Test-Path -Path "c:\veyoninstalado.txt")) {
 
   invoke-expression -Command "cmd /c 'C:\Program Files\veyon\veyon-cli.exe' authkeys import Profesor ${fileCert}"
 
-  echo veyoninstalado> c:/veyoninstalado.txt
+  echo veyoninstalado> c:\veyoninstalado.txt
+}
+
+
+If(-Not (Test-Path -Path "c:\veyoninstalado_fix.txt")) {
+  "C:\Program Files\Veyon\veyon-cli.exe" config set Authentication/Method 0
+  "C:\Program Files\Veyon\veyon-cli.exe" config set AccessControl/AllowRemoteAccess true
+  "C:\Program Files\Veyon\veyon-cli.exe" config set AccessControl/AllowRemoteControl true
+  "C:\Program Files\Veyon\veyon-cli.exe" config set AccessControl/AllowRemoteViewDesktop true
+  "C:\Program Files\Veyon\veyon-cli.exe" config set AccessControl/AllowDesktopCompositing true
+  "C:\Program Files\Veyon\veyon-cli.exe" config set AccessControl/AllowScreenLock true
+  "C:\Program Files\Veyon\veyon-cli.exe" config set AccessControl/AllowPowerControl true
+  "C:\Program Files\Veyon\veyon-cli.exe" config set AccessControl/UserLoginRequired false
+   
+  netsh advfirewall firewall add rule name="Veyon" dir=in action=allow program="C:\Program Files\Veyon\veyon-service.exe" enable=yes
+  netsh advfirewall firewall add rule name="Veyon-Ports" dir=in action=allow protocol=TCP localport=5900,11100,11400 enable=yes
+   
+  net stop VeyonService
+  net start VeyonService
+  
+  echo veyoninstalado> c:\veyoninstalado_fix.txt
 }
